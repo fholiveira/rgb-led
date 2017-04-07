b0051a7b7df080be038fd92a0070c4e031504356
Fernando Oliveira <fernando@fholiveira.com>
2017-04-07 07:57:45 -0300

    Update example

--- examples/Blink.cpp
+++ examples/Blink.cpp
@@ -3,12 +3,10 @@
 
 auto led = RGBLed(ANODE, 9, 10, 11);
 
-Color colors[2] = { Color(0, 255, 0), Color(0, 0, 0) };
-
 void setup() {
     led.setup();
 }
 
 void loop() {
-    led.blink(colors, 500);
+    led.blink(Color(0, 255, 0), 500);
 }
